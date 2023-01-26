7408e87692395d87520224aa8b337e5b95579793
madelein04 <madepanqueva@gmail.com>
2023-01-26 11:09:13 -0500

    timerlisto2

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let inputs, clock, alarm, hours, minutes, seconds, repeater;
 window.addEventListener('load', () => {
     inputs = Array.from(document.getElementsByClassName('number'));
     clock = document.querySelector('.clock');
-    alarm = new Audio('/sound/alarm.mp3');
+    alarm = new Audio('/sound/wind.mp3');
 });
 
 function startTimer() {
